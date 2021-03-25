4d28c5ff3f62797c1761f4c6da0f46ec492c059f
wahyu6070 <gwtsendangrejo6070@gmail.com>
2021-03-25 09:14:47 +0700

    improv cleanup

--- bin/jancox-tool/cleanup.sh
+++ bin/jancox-tool/cleanup.sh
@@ -5,18 +5,31 @@
 
 #PATH
 jancox=`dirname "$(readlink -f $0)"`
-log=$jancox/bin/jancox.log
-loglive=$jancox/bin/jancox.live.log
 #functions
 . $jancox/bin/arm/kopi
-del $loglive && touch $loglive
 clear
-printlog "                Jancox Tool by wahyu6070"
-printlog " "
-printlog "       Cleanup "
-printlog " "
-printlog "- Cleaning"
-[ -d $jancox/bin/tmp ] && del $jancox/bin/tmp
-[ -d $jancox/editor ] && del $jancox/editor
-[ -f  $jancox/new_rom.zip ] && del $jancox/new_rom.zip
-printlog "- Done"
+printmid "${Y}Jancox Tool by wahyu6070${W}"
+print " "
+print "       Cleanup "
+print " "
+print "- Cleaning"
+for RM_DIR in editor; do
+[ -d $jancox/$RM_DIR ] && del $jancox/$RM_DIR && print "   Removing •> $jancox/$RM_DIR"
+done
+for RM_FILES in $(ls -1 $jancox); do
+	case "$RM_FILES" in
+		*new-rom*)
+			print "   Removing •> $jancox/$RM_FILES"
+			rm -rf $jancox/$RM_FILES
+			;;
+	esac
+done
+for RM_BIN in $(ls -1 $jancox/bin); do
+	case $RM_BIN in
+	*log | *tar.gz | tmp)
+	print "   Removing •> $jancox/bin/$RM_BIN"
+	rm -rf $jancox/bin/$RM_BIN
+	;;
+	esac
+done
+print "- Done"
